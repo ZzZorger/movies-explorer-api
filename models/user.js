f203b220755889b8812dc59b00f8d78fa44b375d
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-11-27 21:52:30 +0300

    Настройка моделей

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const emailRegexp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: emailRegexp,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
