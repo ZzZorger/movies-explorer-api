f2b2575fcb04dc40261614638f6fda2968624f6d
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-11-27 21:20:48 +0300

    Создание app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3001 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: false,
+});
+
+app.listen(PORT, () => {
+});
