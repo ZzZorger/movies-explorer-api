4a896c3c70c11576cbca800258d95ef8f7cf2a46
Alexandr Yakushkin <jakushkinn@yandex.ru>
2022-12-08 14:46:33 +0300

    Залив на сервер

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
   useUnifiedTopology: false,
 });
